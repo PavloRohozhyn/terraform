b6c51f4de8c3470bfd4e05d04c5d0d5f15d37175
rohozhyn <pavlo.rohozhyn@gmail.com>
2026-03-27 13:13:42 +0100

    docker section

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,23 +1,27 @@
+#!/bin/bash
+# update index
 sudo apt-get update
-    
-# Docker
+# system doesnt have 
 if ! command -v docker &> /dev/null; then
     echo "Docker not found and vill be installed."
+    # install curl utilite
     sudo apt-get install -y ca-certificates curl gnupg
-    # Set GPG-key
+    # set GPG-key for docker 
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://docker.com | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    # Set official Repository
+    # set repo, create docker list file in to source list folder 
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://docker.com \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    # Installer Docker Engine
+    # after that need to reload data into index
     sudo apt-get update
+    # install docker 
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
     echo "Docker --- Done"
 else
+# docker is already installed 
     echo "Docker already installed. Versjon: $(docker --version)"
 fi
 
